90fab8717be77cb5bef7a6e0713e091fcbe38671
Eduardo Santos <3dmena@gmail.com>
2025-07-25 16:21:52 -0600

    fix(pins) wrong pins in button and flow sensor

--- config/pins.py
+++ config/pins.py
@@ -1,16 +1,17 @@
+# config/pins
 
 from machine import Pin, SoftI2C
 
-COMPRESSOR_A_PIN = 2    # IO2  – Compresor A
+COMPRESSOR_A_PIN = 0    # IO2  – Compresor A
 COMPRESSOR_B_PIN = 4    # IO4  – Compresor B
 PUMP_RELAY_PIN = 14     # IO14 – Rele bomba
 INDICATOR_PIN = 12      # IO12 – Indicador visual
 
-FLOW_SENSOR_PIN = 19    # IO19 – Sensor de flujo
-BUTTON_PIN = 18         # IO18 – Botón
+FLOW_SENSOR_PIN = 18    # IO19 – Sensor de flujo
+BUTTON_PIN = 19         # IO18 – Botón
 
 SCL_PIN = 23
 SDA_PIN = 21
 
-def i2c(bus=0):
-    return SoftI2C(bus, scl=Pin(SCL_PIN), sda=Pin(SDA_PIN))
+def i2c():
+    return SoftI2C(scl=Pin(SCL_PIN), sda=Pin(SDA_PIN))
