b29435d856fc39e7ab7c14d2195f1c45b62fd2ee
Eduardo Santos <3dmena@gmail.com>
2025-07-25 19:37:25 -0600

    feat(boot): updated boot to accept mode selector demo / normal

---
+++ boot.py
@@ -0,0 +1,33 @@
+# boot.py
+import gc
+import machine
+import time
+from config.pins import i2c, DIP_SWITCH_DEMO_PIN
+from utils.drivers.ds3231 import DS3231
+import system_state # Importamos nuestro nuevo módulo
+
+print("\n" + "="*40)
+print("  BIOREACTOR IOT - INICIANDO SISTEMA")
+print("="*40)
+
+dip_demo = machine.Pin(DIP_SWITCH_DEMO_PIN, machine.Pin.IN, machine.Pin.PULL_UP)
+
+if dip_demo.value() == 0:
+    system_state.set_mode('DEMO')
+else:
+    system_state.set_mode('NORMAL')
+
+try:
+    ds = DS3231(i2c())
+    if ds.osf():
+        print("ALERTA: RTC ha perdido energía. La hora puede no ser fiable.")
+    machine.RTC().datetime(ds.datetime())
+    print("RTC Sincronizado:", time.localtime())
+except Exception as e:
+    print(f"ERROR: No se pudo inicializar el RTC DS3231. Detalle: {e}")
+
+gc.collect()
+print("-" * 40)
+print(f"boot.py finalizado. Memoria libre: {gc.mem_free()} bytes.")
+print("Cargando main.py...")
+print("="*40)
